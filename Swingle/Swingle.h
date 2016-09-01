562ddb56a240f43428d8db944a85824a119f9594
Fujii Yosuke <fujiyou1121@gmail.com>
2016-09-01 12:10:50 +0900

    initial commit

---
+++ Swingle/Swingle.h
@@ -0,0 +1,19 @@
+//
+//  Swingle.h
+//  Swingle
+//
+//  Created by 藤井陽介 on 2016/09/01.
+//  Copyright © 2016年 touyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swingle.
+FOUNDATION_EXPORT double SwingleVersionNumber;
+
+//! Project version string for Swingle.
+FOUNDATION_EXPORT const unsigned char SwingleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swingle/PublicHeader.h>
+
+
---
+++ SwingleDemo/SwingleDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwingleDemo
+//
+//  Created by 藤井陽介 on 2016/09/01.
+//  Copyright © 2016年 touyou. All rights reserved.
+//
+
+import UIKit
+import Swingle
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
